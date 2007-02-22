be6f9d320a0ee8adeb4ab059312d26a66b506b7e
cweider <cweider@e965ece2-082a-0410-999d-e21925a5c683>
2007-02-22 04:15:51 +0000

    Initial import of project

---
+++ main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  CTGradient
+//
+//  Created by Chad Weider on 1/15/06.
+//  Copyright (c) 2006 Chad Weider.
+//  Some rights reserved: <http://creativecommons.org/licenses/by/2.5/>
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
